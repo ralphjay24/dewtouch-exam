c49e32712e29b049479bfadd7a2b3f732d1627ed
--global <rjpepito24@gmail.com>
2019-11-28 19:49:06 +0800

    update database.php config

--- app/Config/database.php
+++ app/Config/database.php
@@ -71,7 +71,7 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' 		 => 'localhost:3306',
+		'host'       => 'localhost',
 		'login'      => 'root',
 		'password'   => '',
 		'database'   => 'dewtouch_db',
